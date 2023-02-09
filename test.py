21afe1b855ff9c59239f0f5a3c8bb556c53266e4
Paul Smith <paulm.smith18@gmail.com>
2023-02-09 04:05:42 -0500

    "unnecessary file removed"

--- test.py
+++
@@ -1,5 +0,0 @@
-big_nums =[ 88,14,100,6,80,345,78]
-
-print(f'This is will be my first memorable time using the f function ' +
-      f'\nThis is a list too. {big_nums}' +
-      '\n\tThis is beautiful!')
